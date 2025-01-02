1e4f1df5cdef9dbca618480cfb73d00178a44abf
SungZao <cafecumleti55@gmail.com>
2025-01-02 17:40:47 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+if (window.location.pathname == "/Castilla.html"){
+    function startTimer(duration, display) {
+        var timer = duration, minutes, seconds;
+        setInterval(function () {
+            minutes = parseInt(timer / 60, 10);
+            seconds = parseInt(timer % 60, 10);
+            minutes = minutes < 10 ? "0" + minutes : minutes;
+            seconds = seconds < 10 ? "0" + seconds : seconds;
+            display.textContent = minutes + ":" + seconds;
+            if (--timer < 0) {
+                timer = duration;
+            }
+            if(minutes == 0 && seconds == 0)
+                window.location.replace("enter.html");
+        }, 1000);
+    }
+    window.onload = function () {
+        var duration = 60; // Converter para segundos
+            display = document.getElementById('countdown'); // selecionando o timer
+        startTimer(duration, display); // iniciando o timer
+    };
+}
+
+function login(){
+    const url = window.location.pathname;
+    if(url == "/index.html"){
+        if(document.getElementById("enter").value == "0539")
+            window.location.replace("enter.html");
+        else
+            document.getElementById("mensagem").textContent = "Wayne não identificado!"
+        
+    }else if (url == "/enter.html"){
+        switch(document.getElementById("enter").value){
+            case "Necrose":
+                window.location.replace("Necrose.html");
+            break;
+            case "necrose":
+                window.location.replace("Necrose.html");
+            break;
+            case "Castilla":
+                window.location.replace("Castilla.html");
+            break;
+            case "castilla":
+                window.location.replace("Castilla.html");
+            break;
+        }
+    }
+}
